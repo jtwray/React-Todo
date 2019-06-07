6f5f28b76ee549186f2752cd742e4cc76257cc6b
jtwray <tuckerwray84@gmail.com>
2019-06-07 00:06:12 -0400

    Clicking each task references its unique item id. added onClick listener to div of todoItem component. CallBack references props passed from index.js Class Component's method: toggleItemObj, which is setup to console.log the unique id assigned to each task{object} by a date.now call.

--- src/components/TodoComponents/TodoItem.js
+++ src/components/TodoComponents/TodoItem.js
@@ -3,7 +3,7 @@ import React from "react";
 const TodoItem = props => {
     console.log(props)
   return (
-    <div>
+    <div onClick={()=>props.toggleItemObj(props.itemObj.id)}>
       <p>{props.itemObj.task}</p>
     </div>
   );
